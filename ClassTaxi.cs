abe7c881f52d1bd858fe49825fc305d4f58e2790
aribaqilmuhammad <arib.aqil.muhammad@gmail.com>
2022-03-23 20:52:55 +0700

    ClassTaxi

---
+++ ClassTaxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+    class ClassTaxi
+    {
+        public string DriveName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        // method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("TaxiInfo: {0}", DriveName);
+            Console.WriteLine("PickUpPassenger: {0}", OnDuty);
+            Console.WriteLine("DropOffPassenger: {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} Sedang menjemput penumpang", DriveName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriveName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object Taxi
+            ClassTaxi taxi = new ClassTaxi();
+
+            //pengesetan nilai properties
+            taxi.DriveName = "jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
